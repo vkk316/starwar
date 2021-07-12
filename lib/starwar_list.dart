87028d74e629cfb164bdfb9ac71ba5de7fab1433
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2021-07-12 19:49:27 +0700

    get repo

--- lib/starwar_list.dart
+++ lib/starwar_list.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class StarwarList extends StatelessWidget{
+class StarwarList extends StatefulWidget {
+
+  @override
+  _StarwarListState createState() => _StarwarListState();
+}
+
+class _StarwarListState extends State<StarwarList> {
+  final List<String> entries = <String>['A', 'B', 'C'];
+
+final List<int> colorCodes = <int>[600, 500, 100];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 50,
+            color: Colors.amber[colorCodes[index]],
+            child: Center(child: Text('Entry ${entries[index]}')),
+          );
+        });
   }
 }
